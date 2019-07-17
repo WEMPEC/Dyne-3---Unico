91cc73ea0513615fde7421acfdc86e27faeb5d2d
dongyu9817 <43794945+dongyu9817@users.noreply.github.com>
2019-07-17 17:22:32 -0500

    ver3. abs and parse

--- ParseXml.java
+++ ParseXml.java
@@ -6,21 +6,26 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.*;
 
 public class ParseXml {
-  
+
   private Node absNodeVDD;
   private NodeList absVDDList;
+  private int[] sentVal;
+  private int[] recVal;
 
   public ParseXml(File inputFile) throws ParserConfigurationException {
     // File inputFile = new File(fileName);
+
     DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
     DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
     try {
       Document doc = dBuilder.parse(inputFile);
       doc.getDocumentElement().normalize();
       System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-      
+
       absVDDList = doc.getElementsByTagName("values_to_Drive_Dsp");
       System.out.println("----------------------------");
+      sentVal = new int[absVDDList.getLength()];
+      recVal = new int[absVDDList.getLength()];
 
       for (int temp = 0; temp < absVDDList.getLength(); temp++) {
         Node nNode = absVDDList.item(temp);
@@ -30,7 +35,11 @@ public class ParseXml {
           Element eElement = (Element) nNode;
           String name = eElement.getElementsByTagName("name").item(0).getTextContent();
           String sent = eElement.getElementsByTagName("sent").item(0).getTextContent();
+          sentVal[temp] = Integer.parseInt(sent);
+
           String receive = eElement.getElementsByTagName("received").item(0).getTextContent();
+          recVal[temp] = Integer.parseInt(receive);
+
           System.out
               .println(" Name : " + eElement.getElementsByTagName("name").item(0).getTextContent());
           System.out
@@ -39,12 +48,12 @@ public class ParseXml {
               "received  : " + eElement.getElementsByTagName("received").item(0).getTextContent());
           // System.out.println(
           // "Marks : " + eElement.getElementsByTagName("marks").item(0).getTextContent());
-         // absNodeVDD.appendChild(eElement);
-         
-        
+          // absNodeVDD.appendChild(eElement);
+
+
         }
       }
-      
+
       NodeList limitList = doc.getElementsByTagName("Limits");
       System.out.println("----------------------------");
 
@@ -108,6 +117,34 @@ public class ParseXml {
    */
   public NodeList getAbsVDDList() {
     return absVDDList;
+  }
+
+  /**
+   * @return the sentVal
+   */
+  public int[] getSentVal() {
+    return sentVal;
+  }
+
+  /**
+   * @param sentVal the sentVal to set
+   */
+  public void setSentVal(int[] sentVal) {
+    this.sentVal = sentVal;
+  }
+
+  /**
+   * @return the recVal
+   */
+  public int[] getRecVal() {
+    return recVal;
+  }
+
+  /**
+   * @param recVal the recVal to set
+   */
+  public void setRecVal(int[] recVal) {
+    this.recVal = recVal;
   }
 
 }
