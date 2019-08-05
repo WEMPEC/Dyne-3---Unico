a2ceedfaeecf3a74b798deb90b26a556e917c850
dongyu9817 <43794945+dongyu9817@users.noreply.github.com>
2019-08-05 15:25:44 -0500

    add more features

--- MiddlePanel.java
+++ MiddlePanel.java
@@ -30,8 +30,8 @@ import javax.swing.plaf.basic.BasicSliderUI;
 public class MiddlePanel extends JPanel implements ActionListener {
 
   // creating two buttons
-  private JButton startPiu;
-  private JButton startDrive;
+  private JButton startPiu, startDrive;
+  private JLabel sent, received;
   private StringListener textListener;
 
 
@@ -51,12 +51,12 @@ public class MiddlePanel extends JPanel implements ActionListener {
 
     startPiu = new JButton("Start Piu");
     startPiu.setFont((new Font("Times New Roman", Font.BOLD, 17)));
-    startPiu.setBackground((new Color(0, 211, 0)));
+    startPiu.setBackground(new Color(0, 190, 25));
     startPiu.setForeground(Color.WHITE);
 
     startDrive = new JButton("Start Drive");
     startDrive.setFont((new Font("Times New Roman", Font.BOLD, 17)));
-    startDrive.setBackground(new Color(0, 211, 0));
+    startDrive.setBackground(new Color(0, 190, 25));
     startDrive.setForeground(Color.WHITE);
 
 
@@ -66,7 +66,6 @@ public class MiddlePanel extends JPanel implements ActionListener {
     startPiu.addActionListener(this);
     // flow layout allows to add a group of components.
     setLayout(new FlowLayout(FlowLayout.LEFT));
-
     c.fill = GridBagConstraints.HORIZONTAL;
     // c.weightx = 0.5;
     c.gridx = 0;
@@ -74,7 +73,25 @@ public class MiddlePanel extends JPanel implements ActionListener {
     c.insets = new Insets(22, 0, 0, 0);
     pane.add(startPiu, c);
 
+    sent = new JLabel("     Set");
+    sent.setBorder(BorderFactory.createLineBorder(Color.GREEN));
+    sent.setForeground(new Color(0, 200, 15));
+    sent.setFont(new Font("Times New Roman", Font.BOLD, 20));
+    c.anchor = GridBagConstraints.CENTER;
+    c.gridx = 0;
+    c.gridy = 5;
+    c.insets = new Insets(0, 40, 0, 0);
+    pane.add(sent, c);
 
+    received = new JLabel("Received");
+    received.setBorder(BorderFactory.createLineBorder(Color.magenta));
+    received.setFont(new Font("Times New Roman", Font.BOLD, 20));
+    received.setForeground(Color.magenta);
+    c.anchor = GridBagConstraints.CENTER;
+    c.gridx = 0;
+    c.gridy = 6;
+    c.insets = new Insets(8, 40, 0, 0);
+    pane.add(received, c);
 
     c.fill = GridBagConstraints.HORIZONTAL;
     // c.weightx = 0.5;
@@ -91,57 +108,81 @@ public class MiddlePanel extends JPanel implements ActionListener {
     slider_1.setUI(new MySliderUI(slider_1));
     c.anchor = GridBagConstraints.EAST;
     c.fill = GridBagConstraints.VERTICAL;
-    c.insets = new Insets(0, 0, 0, 35);
-    c.gridx = 0;
+    c.insets = new Insets(0, 0, 0, 40);
+    c.gridx = 1;
     c.gridy = 10;
     pane.add(slider_1, c);
 
     JLabel label_4 = new JLabel("Volt Cmd (V)");
-    label_4.setFont(new Font("Times New Roman", Font.BOLD, 14));
+    label_4.setFont(new Font("Times New Roman", Font.BOLD, 20));
     GridBagConstraints gbc_label_4 = new GridBagConstraints();
     gbc_label_4.fill = GridBagConstraints.BOTH;
     gbc_label_4.anchor = GridBagConstraints.EAST;
     gbc_label_4.insets = new Insets(10, 5, 0, 0);
-    gbc_label_4.gridx = 0;
+    gbc_label_4.gridx = 1;
     gbc_label_4.gridy = 4;
     pane.add(label_4, gbc_label_4);
 
     JSpinner spinner = new JSpinner();
+    spinner.getEditor().getComponent(0).setBackground(Color.green);
     JComponent editor = spinner.getEditor();
     JFormattedTextField tf = ((JSpinner.DefaultEditor) editor).getTextField();
     tf.setColumns(7);
-    spinner.setPreferredSize(new Dimension(70, 50));
+    spinner.setPreferredSize(new Dimension(70, 30));
     spinner.setFont(new Font("Times New Roman", Font.PLAIN, 17));
     spinner.setModel(new SpinnerNumberModel(new Integer(0), null, null, new Integer(1)));
     spinner.setEditor(new JSpinner.NumberEditor(spinner, "     "));
     GridBagConstraints gbc_s1 = new GridBagConstraints();
     gbc_s1.anchor = GridBagConstraints.EAST;
     gbc_s1.insets = new Insets(0, 0, 0, 45);
-    gbc_s1.gridx = 0;
+    gbc_s1.gridx = 1;
     gbc_s1.gridy = 5;
     pane.add(spinner, gbc_s1);
 
-
+    JSpinner spinnera = new JSpinner();
+    JFormattedTextField tfa = ((JSpinner.DefaultEditor) editor).getTextField();
+    tfa.setColumns(7);
+    spinnera.setPreferredSize(new Dimension(70, 30));
+    spinnera.setFont(new Font("Times New Roman", Font.PLAIN, 17));
+    spinnera.setModel(new SpinnerNumberModel(new Integer(0), null, null, new Integer(1)));
+    spinnera.setEditor(new JSpinner.NumberEditor(spinnera, "     "));
+    GridBagConstraints gbc_s1a = new GridBagConstraints();
+    gbc_s1a.anchor = GridBagConstraints.EAST;
+    gbc_s1a.insets = new Insets(10, 0, 0, 45);
+    gbc_s1a.gridx = 1;
+    gbc_s1a.gridy = 6;
+    pane.add(spinnera, gbc_s1a);
 
     JLabel label_8 = new JLabel("Curr Lim (A)");
-    label_8.setFont(new Font("Times New Roman", Font.BOLD, 14));
+    label_8.setFont(new Font("Times New Roman", Font.BOLD, 20));
     GridBagConstraints gbc_label_8 = new GridBagConstraints();
     gbc_label_8.fill = GridBagConstraints.BOTH;
-    gbc_label_8.insets = new Insets(10, 25, 0, 0);
-    gbc_label_8.gridx = 1;
+    gbc_label_8.insets = new Insets(10, 55, 0, 0);
+    gbc_label_8.gridx = 2;
     gbc_label_8.gridy = 4;
     pane.add(label_8, gbc_label_8);
 
     JSpinner spinner1 = new JSpinner();
-    spinner1.setPreferredSize(new Dimension(70, 50));
+    spinner1.setPreferredSize(new Dimension(70, 30));
     spinner1.setFont(new Font("Times New Roman", Font.PLAIN, 17));
     spinner1.setModel(new SpinnerNumberModel(new Integer(0), null, null, new Integer(1)));
     spinner1.setEditor(new JSpinner.NumberEditor(spinner1, "     "));
     GridBagConstraints gbc_s2 = new GridBagConstraints();
-    gbc_s2.insets = new Insets(0, 25, 0, 15);
-    gbc_s2.gridx = 1;
+    gbc_s2.insets = new Insets(0, 55, 0, 15);
+    gbc_s2.gridx = 2;
     gbc_s2.gridy = 5;
     pane.add(spinner1, gbc_s2);
+
+    JSpinner spinner1a = new JSpinner();
+    spinner1a.setPreferredSize(new Dimension(70, 30));
+    spinner1a.setFont(new Font("Times New Roman", Font.PLAIN, 17));
+    spinner1a.setModel(new SpinnerNumberModel(new Integer(0), null, null, new Integer(1)));
+    spinner1a.setEditor(new JSpinner.NumberEditor(spinner1a, "     "));
+    GridBagConstraints gbc_s2a = new GridBagConstraints();
+    gbc_s2a.insets = new Insets(10, 55, 0, 15);
+    gbc_s2a.gridx = 2;
+    gbc_s2a.gridy = 6;
+    pane.add(spinner1a, gbc_s2a);
 
     JSlider slider_2 = new JSlider(SwingConstants.VERTICAL, 0, 150, 0);
     slider_2.setPaintTicks(true);
@@ -152,32 +193,43 @@ public class MiddlePanel extends JPanel implements ActionListener {
     GridBagConstraints gbc_slider_2 = new GridBagConstraints();
     gbc_slider_2.anchor = GridBagConstraints.WEST;
     gbc_slider_2.fill = GridBagConstraints.VERTICAL;
-    gbc_slider_2.insets = new Insets(0, 65, 0, 0);
-    gbc_slider_2.gridx = 1;
+    gbc_slider_2.insets = new Insets(0, 85, 0, 0);
+    gbc_slider_2.gridx = 2;
     gbc_slider_2.gridy = 10;
     pane.add(slider_2, gbc_slider_2);
 
 
 
-    JLabel label_5 = new JLabel("PowerLim (kw)");
-    label_5.setFont(new Font("Times New Roman", Font.BOLD, 14));
+    JLabel label_5 = new JLabel("Speed (rpm)");
+    label_5.setFont(new Font("Times New Roman", Font.BOLD, 20));
     GridBagConstraints gbc_label_5 = new GridBagConstraints();
     gbc_label_5.fill = GridBagConstraints.BOTH;
-    gbc_label_5.insets = new Insets(10, 40, 0, 0);
-    gbc_label_5.gridx = 2;
+    gbc_label_5.insets = new Insets(10, 75, 0, 0);
+    gbc_label_5.gridx = 3;
     gbc_label_5.gridy = 4;
     pane.add(label_5, gbc_label_5);
 
     JSpinner spinner2 = new JSpinner();
-    spinner2.setPreferredSize(new Dimension(70, 50));
+    spinner2.setPreferredSize(new Dimension(70, 30));
     spinner2.setFont(new Font("Times New Roman", Font.PLAIN, 17));
     spinner2.setModel(new SpinnerNumberModel(new Integer(0), null, null, new Integer(1)));
     spinner2.setEditor(new JSpinner.NumberEditor(spinner2, "     "));
     GridBagConstraints gbc_s3 = new GridBagConstraints();
-    gbc_s3.insets = new Insets(0, 20, 0, 15);
-    gbc_s3.gridx = 2;
+    gbc_s3.insets = new Insets(0, 70, 0, 15);
+    gbc_s3.gridx = 3;
     gbc_s3.gridy = 5;
     pane.add(spinner2, gbc_s3);
+
+    JSpinner spinner2a = new JSpinner();
+    spinner2a.setPreferredSize(new Dimension(70, 30));
+    spinner2a.setFont(new Font("Times New Roman", Font.PLAIN, 17));
+    spinner2a.setModel(new SpinnerNumberModel(new Integer(0), null, null, new Integer(1)));
+    spinner2a.setEditor(new JSpinner.NumberEditor(spinner2a, "     "));
+    GridBagConstraints gbc_s3a = new GridBagConstraints();
+    gbc_s3a.insets = new Insets(10, 70, 0, 15);
+    gbc_s3a.gridx = 3;
+    gbc_s3a.gridy = 6;
+    pane.add(spinner2a, gbc_s3a);
 
 
     JSlider slider = new JSlider(JSlider.VERTICAL, 0, 200, 0);
@@ -189,35 +241,45 @@ public class MiddlePanel extends JPanel implements ActionListener {
     slider.setPaintTicks(true);
     slider.setPaintLabels(true);
     slider.setUI(new MySliderUI(slider));
-
     GridBagConstraints gbc_slider = new GridBagConstraints();
     gbc_slider.fill = GridBagConstraints.VERTICAL;
-    gbc_slider.insets = new Insets(0, 50, 0, 0);
-    gbc_slider.gridx = 2;
+    gbc_slider.insets = new Insets(0, 75, 0, 0);
+    gbc_slider.gridx = 3;
     gbc_slider.gridy = 10;
     pane.add(slider, gbc_slider);
 
 
-    JLabel label_6 = new JLabel("IntRes (mOhm)");
-    label_6.setFont(new Font("Times New Roman", Font.BOLD, 14));
+    JLabel label_6 = new JLabel("Torq (Nm)");
+    label_6.setFont(new Font("Times New Roman", Font.BOLD, 20));
     GridBagConstraints gbc_label_6 = new GridBagConstraints();
     gbc_label_6.fill = GridBagConstraints.BOTH;
-    gbc_label_6.insets = new Insets(10, 40, 0, 0);
-    gbc_label_6.gridx = 3;
+    gbc_label_6.insets = new Insets(10, 80, 0, 0);
+    gbc_label_6.gridx = 4;
     gbc_label_6.gridy = 4;
     pane.add(label_6, gbc_label_6);
 
     JSpinner spinner3 = new JSpinner();
-    spinner3.setPreferredSize(new Dimension(70, 50));
+    spinner3.setPreferredSize(new Dimension(70, 30));
     spinner3.setFont(new Font("Times New Roman", Font.PLAIN, 17));
     spinner3.setModel(new SpinnerNumberModel(new Integer(0), null, null, new Integer(1)));
     spinner3.setEditor(new JSpinner.NumberEditor(spinner3, "     "));
     GridBagConstraints gbc_s4 = new GridBagConstraints();
-    gbc_s4.insets = new Insets(0, 30, 0, 15);
-    gbc_s4.gridx = 3;
+    gbc_s4.insets = new Insets(0, 80, 0, 15);
+    gbc_s4.gridx = 4;
     gbc_s4.gridy = 5;
-    spinner3.setEnabled(false);
     pane.add(spinner3, gbc_s4);
+
+    JSpinner spinner3a = new JSpinner();
+    spinner3a.setPreferredSize(new Dimension(70, 30));
+    spinner3a.setFont(new Font("Times New Roman", Font.PLAIN, 17));
+    spinner3a.setModel(new SpinnerNumberModel(new Integer(0), null, null, new Integer(1)));
+    spinner3a.setEditor(new JSpinner.NumberEditor(spinner3a, "     "));
+    GridBagConstraints gbc_s4a = new GridBagConstraints();
+    gbc_s4a.insets = new Insets(10, 80, 0, 15);
+    gbc_s4a.gridx = 4;
+    gbc_s4a.gridy = 6;
+    pane.add(spinner3a, gbc_s4a);
+
 
     JSlider slider_4 = new JSlider(SwingConstants.VERTICAL, 0, 30, 15);
     slider_4.setPaintTicks(true);
@@ -228,8 +290,8 @@ public class MiddlePanel extends JPanel implements ActionListener {
     GridBagConstraints gbc_slider_4 = new GridBagConstraints();
     gbc_slider_4.anchor = GridBagConstraints.WEST;
     gbc_slider_4.fill = GridBagConstraints.VERTICAL;
-    gbc_slider_4.insets = new Insets(0, 70, 0, 0);
-    gbc_slider_4.gridx = 3;
+    gbc_slider_4.insets = new Insets(0, 105, 0, 0);
+    gbc_slider_4.gridx = 4;
     gbc_slider_4.gridy = 10;
     pane.add(slider_4, gbc_slider_4);
 
