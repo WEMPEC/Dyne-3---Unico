7b3471dac757819364ff7990c72947b22ba5e49e
dongyu9817 <43794945+dongyu9817@users.noreply.github.com>
2019-08-16 17:11:55 -0500

    Retrive Piu tab value

--- TabPiu.java
+++ TabPiu.java
@@ -46,334 +46,41 @@ public class TabPiu extends JPanel {
   private JTable table;
   private GridBagLayout gbl_panel_2;
   private JPanel panel_2;
-  private GridBagConstraints gbc_s1, gbc_s2, gbc_s3, gbc_s4, gbc_slider_1, gbc_slider, gbc_slider_2,
-      gbc_slider_4;
-  private JSlider slider;
+  private String[] tempLimit;
+  private Boolean checkload;
+  private String data[][];
 
-  /**
-   * @return the slider
-   */
-  public JSlider getSlider() {
-    return slider;
-  }
-
-  public TabPiu(JPanel panel, GridBagLayout gl_panel, JPanel panel1, GridBagLayout gl_panel1) {
+  public TabPiu(JPanel panel1, GridBagLayout gl_panel1, Boolean checkload, ParseXml loadinfo) {
     // TODO Auto-generated constructor stub
-    this.gl_Detail = gl_panel;
-    this.panel3 = panel;
     panel_2 = panel1;
     gbl_panel_2 = gl_panel1;
-
-
-    gl_Detail.columnWidths = new int[] {120, 90, 90, 120, 0, 0, 0, 0, 0, 0};
-    gl_Detail.rowHeights = new int[] {30, 35, 26, 8, 22, 50, 0, 0, 0, 0, 514, 0};
-    gl_Detail.columnWeights =
-        new double[] {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-    gl_Detail.rowWeights =
-        new double[] {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-    panel3.setLayout(gl_Detail);
-
-
-    JLabel lblNewLabel_2 = new JLabel("Vslew Cmd");
-    lblNewLabel_2.setFont(new Font("Times New Roman", Font.BOLD, 17));
-    GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
-    // gbc_lblNewLabel_2.anchor = GridBagConstraints.NORTH;
-    gbc_lblNewLabel_2.fill = GridBagConstraints.HORIZONTAL;
-    // gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
-    // gbc_lblNewLabel_2.gridwidth = 2;
-    gbc_lblNewLabel_2.gridx = 0;
-    gbc_lblNewLabel_2.gridy = 1;
-    panel3.add(lblNewLabel_2, gbc_lblNewLabel_2);
-
-    textField_8 = new JTextField("");
-    GridBagConstraints gbc_textField_1 = new GridBagConstraints();
-    textField_8.setColumns(2);// gbc_textField_1.insets = new Insets(0, 0, 5, 5);
-    gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
-    gbc_textField_1.gridx = 1;
-    gbc_textField_1.gridy = 1;
-    panel3.add(textField_8, gbc_textField_1);
-
-
-    JLabel lblNewLabel_1 = new JLabel("Vslew Act");
-    GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
-    lblNewLabel_1.setFont(new Font("Times New Roman", Font.BOLD, 17));
-    gbc_lblNewLabel_1.fill = GridBagConstraints.HORIZONTAL;
-    // gbc_lblNewLabel_1.insets = new Insets(0, 0, 0, 0);
-    gbc_lblNewLabel_1.gridx = 0;
-    gbc_lblNewLabel_1.gridy = 2;
-    panel3.add(lblNewLabel_1, gbc_lblNewLabel_1);
-
-    textField_9 = new JTextField("");
-    GridBagConstraints gbc_textField_9 = new GridBagConstraints();
-    gbc_textField_1.insets = new Insets(0, 0, 0, 0);
-    gbc_textField_9.fill = GridBagConstraints.HORIZONTAL;
-    gbc_textField_9.gridx = 1;
-    gbc_textField_9.gridy = 2;
-    panel3.add(textField_9, gbc_textField_9);
-    textField_9.setColumns(5);
-
-    JLabel lblNewLabel_3 = new JLabel("Lim Act");
-    GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
-    lblNewLabel_3.setFont(new Font("Times New Roman", Font.BOLD, 17));
-    gbc_lblNewLabel_3.fill = GridBagConstraints.HORIZONTAL;
-    gbc_lblNewLabel_3.insets = new Insets(0, 5, 0, 0);
-    gbc_lblNewLabel_3.gridx = 2;
-    gbc_lblNewLabel_3.gridy = 2;
-    panel3.add(lblNewLabel_3, gbc_lblNewLabel_3);
-
-    textField_10 = new JTextField("");
-    GridBagConstraints gbc_textField_10 = new GridBagConstraints();
-    gbc_textField_10.insets = new Insets(0, 70, 0, 0);
-    gbc_textField_10.fill = GridBagConstraints.HORIZONTAL;
-    gbc_textField_10.gridx = 2;
-    gbc_textField_10.gridy = 2;
-    panel3.add(textField_10, gbc_textField_10);
-    textField_10.setColumns(5);
-
-    JToggleButton tglbtnNewToggleButton = new JToggleButton("Volt Mode");
-    GridBagConstraints gbc_tglbtnNewToggleButton = new GridBagConstraints();
-    gbc_tglbtnNewToggleButton.anchor = GridBagConstraints.WEST;
-    // gbc_tglbtnNewToggleButton.fill = GridBagConstraints.HORIZONTAL;
-    gbc_tglbtnNewToggleButton.insets = new Insets(0, 30, 0, 0);
-    gbc_tglbtnNewToggleButton.gridx = 3;
-    gbc_tglbtnNewToggleButton.gridy = 2;
-    panel3.add(tglbtnNewToggleButton, gbc_tglbtnNewToggleButton);
-
-
-    JSeparator separator = new JSeparator();
-    separator.setBackground(new Color(0, 0, 0));
-    GridBagConstraints gbc_separator = new GridBagConstraints();
-    gbc_separator.fill = GridBagConstraints.BOTH;
-    // gbc_separator.insets = new Insets(0, 0, 5, 5);
-    gbc_separator.gridwidth = 6;
-    gbc_separator.gridx = 0;
-    gbc_separator.gridy = 4;
-    panel3.add(separator, gbc_separator);
-
-
-    /// division
-    JLabel label_4 = new JLabel("Volt Cmd (V)");
-    label_4.setFont(new Font("Times New Roman", Font.BOLD, 14));
-    GridBagConstraints gbc_label_4 = new GridBagConstraints();
-    gbc_label_4.fill = GridBagConstraints.BOTH;
-    gbc_label_4.anchor = GridBagConstraints.EAST;
-    gbc_label_4.insets = new Insets(10, 5, 0, 0);
-    gbc_label_4.gridx = 0;
-    gbc_label_4.gridy = 4;
-    panel3.add(label_4, gbc_label_4);
-
-    JSpinner spinner = new JSpinner();
-    JComponent editor = spinner.getEditor();
-    JFormattedTextField tf = ((JSpinner.DefaultEditor) editor).getTextField();
-    tf.setColumns(7);
-    spinner.setPreferredSize(new Dimension(150, 100));
-    spinner.setFont(new Font("Times New Roman", Font.PLAIN, 17));
-    spinner.setModel(new SpinnerNumberModel(new Integer(0), null, null, new Integer(1)));
-    spinner.setEditor(new JSpinner.NumberEditor(spinner, "000"));
-    gbc_s1 = new GridBagConstraints();
-    gbc_s1.anchor = GridBagConstraints.EAST;
-    gbc_s1.insets = new Insets(0, 0, 0, 45);
-    gbc_s1.gridx = 0;
-    gbc_s1.gridy = 5;
-    spinner.setEnabled(false);
-    panel3.add(spinner, gbc_s1);
-
-
-
-    JSlider slider_1 = new JSlider(SwingConstants.VERTICAL, 0, 250, 0);
-    slider_1.setPaintTicks(true);
-    slider_1.setPaintLabels(true);
-    slider_1.setMinorTickSpacing(10);
-    slider_1.setMajorTickSpacing(50);
-    slider_1.setUI(new MySliderUI(slider_1));
-    gbc_slider_1 = new GridBagConstraints();
-    gbc_slider_1.anchor = GridBagConstraints.EAST;
-    gbc_slider_1.fill = GridBagConstraints.VERTICAL;
-    gbc_slider_1.insets = new Insets(0, 0, 0, 35);
-    gbc_slider_1.gridx = 0;
-    gbc_slider_1.gridy = 10;
-    panel3.add(slider_1, gbc_slider_1);
-    slider_1.setEnabled(false);
-
-    spinner.addChangeListener(new ChangeListener() {
-      @Override
-      public void stateChanged(ChangeEvent e) {
-        String tempint = spinner.getValue().toString();
-        if (!tempint.equals(null)) {
-          System.out.println("in listener     ");
-          setSlider(slider_1, tempint);
-          MiddlePanel newMiddle = new MiddlePanel();
-          newMiddle.listener(spinner, slider_1, 0);
-        }
+    this.checkload = checkload;
+    // elements
+    if (checkload & loadinfo != null) {
+      tempLimit = loadinfo.getLimitPiu();
+      // absVDD = loadinfo.getAbsVDDList();
+    } else {
+      tempLimit = new String[15];
+      for (int i = 0; i < tempLimit.length; ++i) {
+        tempLimit[i] = "0";
       }
     }
 
-    );
-    slider_1.addChangeListener(new ChangeListener() {
-      @Override
-      public void stateChanged(ChangeEvent e) {
-        int tempint = slider_1.getValue();
-        if (tempint != 0) {
-          System.out.print("in listener     ");
-          MiddlePanel newMiddle = new MiddlePanel();
 
-          newMiddle.setSpinner(spinner, tempint);
-          setSpinner(spinner, tempint);
-        }
-      }
-    }
-
-    );
-
-
-    JLabel label_8 = new JLabel("Curr Lim (A)");
-    label_8.setFont(new Font("Times New Roman", Font.BOLD, 14));
-    GridBagConstraints gbc_label_8 = new GridBagConstraints();
-    gbc_label_8.fill = GridBagConstraints.BOTH;
-    gbc_label_8.insets = new Insets(10, 25, 0, 0);
-    gbc_label_8.gridx = 1;
-    gbc_label_8.gridy = 4;
-    panel3.add(label_8, gbc_label_8);
-
-    JSpinner spinner1 = new JSpinner();
-    spinner1.setPreferredSize(new Dimension(150, 100));
-    spinner1.setFont(new Font("Times New Roman", Font.PLAIN, 17));
-    spinner1.setModel(new SpinnerNumberModel(new Integer(0), null, null, new Integer(1)));
-    spinner1.setEditor(new JSpinner.NumberEditor(spinner1, "000"));
-    gbc_s2 = new GridBagConstraints();
-    gbc_s2.insets = new Insets(0, 25, 0, 15);
-    gbc_s2.gridx = 1;
-    gbc_s2.gridy = 5;
-    spinner1.setEnabled(false);
-    panel3.add(spinner1, gbc_s2);
-
-    JSlider slider_2 = new JSlider(SwingConstants.VERTICAL, 0, 150, 0);
-    slider_2.setPaintTicks(true);
-    slider_2.setPaintLabels(true);
-    slider_2.setMinorTickSpacing(10);
-    slider_2.setMajorTickSpacing(50);
-    slider_2.setUI(new MySliderUI(slider_2));
-    gbc_slider_2 = new GridBagConstraints();
-    gbc_slider_2.anchor = GridBagConstraints.WEST;
-    gbc_slider_2.fill = GridBagConstraints.VERTICAL;
-    gbc_slider_2.insets = new Insets(0, 65, 0, 0);
-    gbc_slider_2.gridx = 1;
-    gbc_slider_2.gridy = 10;
-    slider_2.setEnabled(false);
-    panel3.add(slider_2, gbc_slider_2);
-    listener(spinner1, slider_2);
-
-    JLabel label_5 = new JLabel("Speed (rpm)");
-    label_5.setFont(new Font("Times New Roman", Font.BOLD, 14));
-    GridBagConstraints gbc_label_5 = new GridBagConstraints();
-    gbc_label_5.fill = GridBagConstraints.BOTH;
-    gbc_label_5.insets = new Insets(10, 40, 0, 0);
-    gbc_label_5.gridx = 2;
-    gbc_label_5.gridy = 4;
-    panel3.add(label_5, gbc_label_5);
-
-    JSpinner spinner2 = new JSpinner();
-    spinner2.setPreferredSize(new Dimension(150, 100));
-    spinner2.setFont(new Font("Times New Roman", Font.PLAIN, 17));
-    spinner2.setModel(new SpinnerNumberModel(new Integer(0), null, null, new Integer(1)));
-    spinner2.setEditor(new JSpinner.NumberEditor(spinner2, "000"));
-    gbc_s3 = new GridBagConstraints();
-    gbc_s3.insets = new Insets(0, 20, 0, 15);
-    gbc_s3.gridx = 2;
-    gbc_s3.gridy = 5;
-    panel3.add(spinner2, gbc_s3);
-
-
-    slider = new JSlider(JSlider.VERTICAL, 0, 200, 0);
-    // slider.setLabelTable(labelTable);
-    slider.setPaintLabels(true);
-    // Turn on labels at major tick marks.
-    slider.setMajorTickSpacing(50);
-    slider.setMinorTickSpacing(10);
-    slider.setPaintTicks(true);
-    slider.setPaintLabels(true);
-    slider.setUI(new MySliderUI(slider));
-
-    gbc_slider = new GridBagConstraints();
-    gbc_slider.fill = GridBagConstraints.VERTICAL;
-    gbc_slider.insets = new Insets(0, 50, 0, 0);
-    gbc_slider.gridx = 2;
-    gbc_slider.gridy = 10;
-    panel3.add(slider, gbc_slider);
-
-    listener(spinner2, slider);
-
-
-    JLabel label_6 = new JLabel("Torq (rpm)");
-    label_6.setFont(new Font("Times New Roman", Font.BOLD, 14));
-    GridBagConstraints gbc_label_6 = new GridBagConstraints();
-    gbc_label_6.fill = GridBagConstraints.BOTH;
-    gbc_label_6.insets = new Insets(10, 40, 0, 0);
-    gbc_label_6.gridx = 3;
-    gbc_label_6.gridy = 4;
-    panel3.add(label_6, gbc_label_6);
-
-    JSpinner spinner3 = new JSpinner();
-    spinner3.setPreferredSize(new Dimension(150, 100));
-    spinner3.setFont(new Font("Times New Roman", Font.PLAIN, 17));
-    spinner3.setModel(new SpinnerNumberModel(new Integer(0), null, null, new Integer(1)));
-    spinner3.setEditor(new JSpinner.NumberEditor(spinner3, "000"));
-    GridBagConstraints gbc_s4 = new GridBagConstraints();
-    gbc_s4.insets = new Insets(0, 30, 0, 15);
-    gbc_s4.gridx = 3;
-    gbc_s4.gridy = 5;
-    spinner3.setEnabled(false);
-    panel3.add(spinner3, gbc_s4);
-
-    JSlider slider_4 = new JSlider(SwingConstants.VERTICAL, 0, 30, 15);
-    slider_4.setPaintTicks(true);
-    slider_4.setPaintLabels(true);
-    slider_4.setMinorTickSpacing(1);
-    slider_4.setMajorTickSpacing(10);
-    slider_4.setUI(new MySliderUI(slider_4));
-    GridBagConstraints gbc_slider_4 = new GridBagConstraints();
-    gbc_slider_4.anchor = GridBagConstraints.WEST;
-    gbc_slider_4.fill = GridBagConstraints.VERTICAL;
-    gbc_slider_4.insets = new Insets(0, 70, 0, 0);
-    gbc_slider_4.gridx = 3;
-    gbc_slider_4.gridy = 10;
-    slider_4.setEnabled(false);
-    panel3.add(slider_4, gbc_slider_4);
-
-    listener(spinner3, slider_4);
-
-
-    tglbtnNewToggleButton.addActionListener(new ActionListener() {
-
-      @Override
-      public void actionPerformed(ActionEvent arg0) {
-        // TODO Auto-generated method stub
-        boolean isclicked = tglbtnNewToggleButton.isSelected();
-        label_4.setEnabled(isclicked);
-        label_8.setEnabled(isclicked);
-        label_6.setEnabled(isclicked);
-        slider_1.setEnabled(isclicked);
-        slider_2.setEnabled(isclicked);
-        slider_4.setEnabled(isclicked);
-        spinner.setEnabled(isclicked);
-        spinner1.setEnabled(isclicked);
-        spinner3.setEnabled(isclicked);
-      }
-
-    });
-
-
-
-    String data[][] = {{"Name", "Set", "Min", "Max"}, {"Min Speed (rpm) ", "?", "0", "10000"},
-        {"Def Speed (rpm) ", "?", "0", "10000"}, {"Max Speed (rpm) ", "?", "0", "1000"},
-        {"Min Torque(Nm) ", "?", "0", "10000"}, {"Def Torque (Nm) ", "?", "0", "10000"},
-        {"Max Torque (Nm) ", "?", "0", "1000"},
-        {"Min Acceleration Limit (rpm/s)", "?", "0", "10000"},
-        {"Def Acceleration Limit (rpm/s)", "?", "0", "10000"},
-        {"Max Acceleration Limit (rpm/s)", "?", "0", "70"},
-        {"Minium Inertia (kgm2)", "?", "0", "70"}, {"Default Inertia (kgm2)", "?", "0", "700"},
-        {"Max Inertia (kgm2)", "?", "0", "70"}};
-    String column[] = {"ID", "NAME", "?", "SALARY"};
+    data = new String[][] {{"Name", "Set", "Min", "Max"},
+        {"Min Speed (rpm) ", tempLimit[0], "0", "10000"},
+        {"Def Speed (rpm) ", tempLimit[1], "0", "10000"},
+        {"Max Speed (rpm) ", tempLimit[2], "0", "1000"},
+        {"Min Torque(Nm) ", tempLimit[3], "0", "10000"},
+        {"Def Torque (Nm) ", tempLimit[4], "0", "10000"},
+        {"Max Torque (Nm) ", tempLimit[5], "0", "1000"},
+        {"Min Acceleration Limit (rpm/s)", tempLimit[6], "0", "10000"},
+        {"Def Acceleration Limit (rpm/s)", tempLimit[7], "0", "10000"},
+        {"Max Acceleration Limit (rpm/s)", tempLimit[8], "0", "70"},
+        {"Minium Inertia (kgm2)", tempLimit[9], "0", "70"},
+        {"Default Inertia (kgm2)", tempLimit[10], "0", "700"},
+        {"Max Inertia (kgm2)", tempLimit[11], "0", "70"}};
+    String column[] = {"ID", "NAME", "SALARY", "set"};
     isCellEditable(4, 4);
     // = new GridBagLayout();
     gbl_panel_2.columnWidths = new int[] {600, 0};
@@ -384,7 +91,7 @@ public class TabPiu extends JPanel {
 
 
     // = new GridBagLayout();
-    JTable table = new JTable(data, column) {
+     table = new JTable(data, column) {
       @Override
       public boolean isCellEditable(int row, int column) {
         return column == 1 ? true : false;
@@ -404,19 +111,6 @@ public class TabPiu extends JPanel {
     gbc_table.gridy = 0;
     panel_2.add(table, gbc_table);
 
-
-
-    //
-    // table.setColumnSelectionAllowed(true);
-    //
-    // table.getColumnModel().getColumn(0).setPreferredWidth(300);
-    // table.getColumnModel().getColumn(1).setPreferredWidth(180);
-    // table.getColumnModel().getColumn(3).setPreferredWidth(220);
-    // GridBagConstraints gbc_table = new GridBagConstraints();
-    // gbc_table.fill = GridBagConstraints.BOTH;
-    // gbc_table.gridx = 0;
-    // gbc_table.gridy = 0;
-    // panel_2.add(table, gbc_table);
 
   }
 
@@ -489,53 +183,41 @@ public class TabPiu extends JPanel {
 
   }
 
-  private void listener(JSpinner spinner, JSlider slider_1) {
-    spinner.addChangeListener(new ChangeListener() {
-      @Override
-      public void stateChanged(ChangeEvent e) {
-        String tempint = spinner.getValue().toString();
-        if (!tempint.equals(null)) {
-          System.out.println("in listener     ");
-          setSlider(slider_1, tempint);
-          MiddlePanel newMiddle = new MiddlePanel();
-          newMiddle.setSlider(slider_1, tempint);
-        }
-      }
+  public String getsavePIU() {
+    String[] listVal = new String[20];
+    for (int i = 1; i < data.length; ++i) {
+      listVal[i - 1] = table.getModel().getValueAt(i, 1).toString();
+
     }
 
-    );
-    slider_1.addChangeListener(new ChangeListener() {
-      @Override
-      public void stateChanged(ChangeEvent e) {
-        int tempint = slider_1.getValue();
-        if (tempint != 0) {
-          System.out.print("in listener     ");
-          MiddlePanel newMiddle = new MiddlePanel();
 
-          newMiddle.setSpinner(spinner, tempint);
-          setSpinner(spinner, tempint);
-        }
-      }
-    }
-
-    );
-
+    String piuinfo =
+        "\n<PIU>\r\n" + "    <LimitsPiu>\r\n" + "        <name>Min Speed (rpm)</name>\r\n"
+            + "        <value>" + listVal[0] + "</value>\r\n" + "    </LimitsPiu>\r\n" + "\r\n"
+            + "    <LimitsPiu>\r\n" + "        <name>Def Speed (rpm)</name>\r\n" + "        <value>"
+            + listVal[1] + "</value>\r\n" + "    </LimitsPiu>\r\n" + "\r\n" + "    <LimitsPiu>\r\n"
+            + "        <name>Max Speed (rpm)</name>\r\n" + "        <value>" + listVal[2]
+            + "</value>\r\n" + "    </LimitsPiu>\r\n" + "\r\n" + "    <LimitsPiu>\r\n"
+            + "        <name>Min Torque (Nm)</name>\r\n" + "        <value>" + listVal[3]
+            + "</value>\r\n" + "    </LimitsPiu>\r\n" + "\r\n" + "    <LimitsPiu>\r\n"
+            + "        <name>Def Torque (Nm)</name>\r\n" + "        <value>" + listVal[4]
+            + "</value>\r\n" + "    </LimitsPiu>\r\n" + "\r\n" + "    <LimitsPiu>\r\n"
+            + "        <name>Max Torque (Nm)</name>\r\n" + "        <value>" + listVal[5]
+            + "</value>\r\n" + "    </LimitsPiu>\r\n" + "\r\n" + "    <LimitsPiu>\r\n"
+            + "        <name>Min AccelLim (rpm/s)</name>\r\n" + "        <value>" + listVal[6]
+            + "</value>\r\n" + "    </LimitsPiu>\r\n" + "\r\n" + "    <LimitsPiu>\r\n"
+            + "        <name>Def AccelLim (rpm/s)</name>\r\n" + "        <value>" + listVal[7]
+            + "</value>\r\n" + "    </LimitsPiu>\r\n" + "\r\n" + "    <LimitsPiu>\r\n"
+            + "        <name>Max AccelLim (rpm/s)</name>\r\n" + "        <value>" + listVal[8]
+            + "</value>\r\n" + "    </LimitsPiu>\r\n" + "\r\n" + "    <LimitsPiu>\r\n"
+            + "        <name>Min Interia (kgm2)</name>\r\n" + "        <value>" + listVal[9]
+            + "</value>\r\n" + "    </LimitsPiu>\r\n" + "\r\n" + "    <LimitsPiu>\r\n"
+            + "        <name>Default Interia (kgm2)</name>\r\n" + "        <value>" + listVal[10]
+            + "</value>\r\n" + "    </LimitsPiu>\r\n" + "\r\n" + "    <LimitsPiu>\r\n"
+            + "        <name>Max Interia (kgm2)</name>\r\n" + "        <value>" + listVal[11]
+            + "</value>\r\n" + "    </LimitsPiu>\r\n" + "    \r\n";
+    return piuinfo;
   }
 
-  public ChangeListener getChangeLister(JSlider slider_1, JSpinner spinner) {
-    ChangeListener e = new ChangeListener() {
-      @Override
-      public void stateChanged(ChangeEvent e) {
-        int tempint = slider_1.getValue();
-        if (tempint != 0) {
-          System.out.print("in listener     ");
-          MiddlePanel newMiddle = new MiddlePanel();
 
-          newMiddle.setSpinner(spinner, tempint);
-          setSpinner(spinner, tempint);
-        }
-      }
-    };
-    return e;
-  }
 }
