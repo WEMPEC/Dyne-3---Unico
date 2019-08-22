81763d01aaf77cd7897416a4a4a98e489b6f6e9f
dongyu9817 <43794945+dongyu9817@users.noreply.github.com>
2019-08-21 23:53:14 -0500

    implement feed slide bar

--- MiddlePanel.java
+++ MiddlePanel.java
@@ -32,34 +32,35 @@ import javax.swing.plaf.basic.BasicSliderUI;
 public class MiddlePanel extends JPanel implements ActionListener {
 
   // creating two buttons
-  private JButton startPiu, startDrive;
+  private JButton startPiu, startDrive, reset;
   private JLabel sent, received;
   private StringListener textListener;
   private JSpinner spinner, spinnera, spinner1, spinner1a, spinner2, spinner2a, spinner3, spinner3a;
-  private JSlider slider, slider_1, slider_2, slider_4;
+  private JSlider slider, slider_1, slider_2, slider_4, slider_1a, slider_2a, slidera, slider_4a;
   private String[] slideVal;
   private Boolean checkload;
 
   public MiddlePanel(ParseXml loadinfo) {
+    slideVal = new String[15];
+    for (int i = 0; i < slideVal.length; ++i) {
+      slideVal[i] = "0";
+    }
+    checkload = false;
     if (loadinfo != null) {
       checkload = true;
       slideVal = loadinfo.getBararray();
       // absVDD = loadinfo.getAbsVDDList();
-    } else {
-      slideVal = new String[4];
-      for (int i = 0; i < slideVal.length; ++i) {
-        slideVal[i] = "0";
-      }
-      checkload = false;
+      System.out.println("loadinfo" + loadinfo.toString());
     }
 
-
+    reset = new JButton("RESET");
+    reset.setFont((new Font("Times New Roman", Font.BOLD, 17)));
 
     setBorder(BorderFactory.createBevelBorder(ABORT, getForeground(), getBackground()));
     JPanel pane = new JPanel(new GridBagLayout());
     GridBagConstraints c = new GridBagConstraints();
     GridBagLayout gl_Detail = new GridBagLayout();
-    gl_Detail.columnWidths = new int[] {120, 90, 90, 120, 0, 0, 0, 0, 0, 0};
+    gl_Detail.columnWidths = new int[] {120, 120, 120, 120, 0, 0, 0, 0, 0, 0};
     gl_Detail.rowHeights = new int[] {30, 35, 26, 8, 22, 20, 0, 0, 0, 0, 514, 0};
     gl_Detail.columnWeights =
         new double[] {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
@@ -91,6 +92,16 @@ public class MiddlePanel extends JPanel implements ActionListener {
     c.insets = new Insets(22, 0, 0, 0);
     pane.add(startPiu, c);
 
+    reset.addActionListener(this);
+    // flow layout allows to add a group of components.
+    setLayout(new FlowLayout(FlowLayout.LEFT));
+    c.fill = GridBagConstraints.HORIZONTAL;
+    // c.weightx = 0.5;
+    c.gridx = 1;
+    c.gridy = 0;
+    c.insets = new Insets(22, 10, 0, 0);
+    pane.add(reset, c);
+
     sent = new JLabel("     Command");
     sent.setBorder(BorderFactory.createLineBorder(Color.GREEN));
     sent.setForeground(new Color(0, 200, 15));
@@ -117,7 +128,7 @@ public class MiddlePanel extends JPanel implements ActionListener {
     c.gridy = 1;
     c.insets = new Insets(15, 0, 0, 0);
     pane.add(startDrive, c);
-
+    System.out.println("slideval in middle"+slideVal[0]);
     slider_1 = new JSlider(SwingConstants.VERTICAL, 0, 250, Integer.parseInt(slideVal[0]));
     slider_1.setPaintTicks(true);
     slider_1.setPaintLabels(true);
@@ -126,7 +137,7 @@ public class MiddlePanel extends JPanel implements ActionListener {
     slider_1.setUI(new MySliderUI(slider_1));
     c.anchor = GridBagConstraints.EAST;
     c.fill = GridBagConstraints.VERTICAL;
-    c.insets = new Insets(0, 0, 0, 40);
+    c.insets = new Insets(0, 0, 0, 70);
     c.gridx = 1;
     c.gridy = 10;
     setSlider(slider_1, slideVal[0]);
@@ -175,8 +186,26 @@ public class MiddlePanel extends JPanel implements ActionListener {
     gbc_s1a.gridx = 1;
     gbc_s1a.gridy = 6;
     pane.add(spinnera, gbc_s1a);
-
+    
+    slider_1a = new JSlider(SwingConstants.VERTICAL, 0, 250, Integer.parseInt(slideVal[0]));
+    slider_1a.setPaintTicks(true);
+    slider_1a.setPaintLabels(true);
+    slider_1a.setMinorTickSpacing(10);
+    slider_1a.setMajorTickSpacing(50);
+    slider_1a.setUI(new MySliderUIBack(slider_1a));
+    GridBagConstraints gbc_sa = new GridBagConstraints();   
+    gbc_sa.anchor = GridBagConstraints.EAST;
+    gbc_sa.fill = GridBagConstraints.VERTICAL;
+    gbc_sa.insets = new Insets(0, 0, 0, 10);
+    gbc_sa.gridx = 1;
+    gbc_sa.gridy = 10;
+    setSlider(slider_1a, slideVal[0]);
+    pane.add(slider_1a, gbc_sa);
+    
+    
     listener(spinner, slider_1, slideVal[0]);
+    listener(spinnera, slider_1a, slideVal[0]);
+    
     ////////////////////////////////////////////////////////////////////
     JLabel label_8 = new JLabel("Curr Lim (A)");
     label_8.setFont(new Font("Times New Roman", Font.BOLD, 20));
@@ -223,12 +252,28 @@ public class MiddlePanel extends JPanel implements ActionListener {
     GridBagConstraints gbc_slider_2 = new GridBagConstraints();
     gbc_slider_2.anchor = GridBagConstraints.WEST;
     gbc_slider_2.fill = GridBagConstraints.VERTICAL;
-    gbc_slider_2.insets = new Insets(0, 85, 0, 0);
+    gbc_slider_2.insets = new Insets(0, 65, 0, 0);
     gbc_slider_2.gridx = 2;
     gbc_slider_2.gridy = 10;
     pane.add(slider_2, gbc_slider_2);
+    
+    slider_2a = new JSlider(SwingConstants.VERTICAL, 0, 150, 0);
+    slider_2a.setPaintTicks(true);
+    slider_2a.setPaintLabels(true);
+    slider_2a.setMinorTickSpacing(10);
+    slider_2a.setMajorTickSpacing(50);
+    slider_2a.setUI(new MySliderUIBack(slider_2a));
+    setSlider(slider_2a, slideVal[1]);
+    GridBagConstraints gbc_slider_2a = new GridBagConstraints();
+    gbc_slider_2a.anchor = GridBagConstraints.WEST;
+    gbc_slider_2a.fill = GridBagConstraints.VERTICAL;
+    gbc_slider_2a.insets = new Insets(0, 125, 0, 0);
+    gbc_slider_2a.gridx = 2;
+    gbc_slider_2a.gridy = 10;
+    pane.add(slider_2a, gbc_slider_2a);
 
     listener(spinner1, slider_2, slideVal[1]);
+    listener(spinner1a, slider_2a, slideVal[1]);
 
     ///////////////////////////////////////////////////////////////
     JLabel label_5 = new JLabel("Speed (rpm)");
@@ -275,12 +320,31 @@ public class MiddlePanel extends JPanel implements ActionListener {
     slider.setUI(new MySliderUI(slider));
     GridBagConstraints gbc_slider = new GridBagConstraints();
     gbc_slider.fill = GridBagConstraints.VERTICAL;
-    gbc_slider.insets = new Insets(0, 75, 0, 0);
+    gbc_slider.insets = new Insets(0, 15, 0, 0);
     gbc_slider.gridx = 3;
     gbc_slider.gridy = 10;
     pane.add(slider, gbc_slider);
     setSlider(slider, slideVal[2]);
+    
+    slidera = new JSlider(JSlider.VERTICAL, 0, 200, 0);
+    // slider.setLabelTable(labelTable);
+    slidera.setPaintLabels(true);
+    // Turn on labels at major tick marks.
+    slidera.setMajorTickSpacing(50);
+    slidera.setMinorTickSpacing(10);
+    slidera.setPaintTicks(true);
+    slidera.setPaintLabels(true);
+    slidera.setUI(new MySliderUIBack(slidera));
+    GridBagConstraints gbc_slidera = new GridBagConstraints();
+    gbc_slidera.fill = GridBagConstraints.VERTICAL;
+    gbc_slidera.insets = new Insets(0, 125, 0, 0);
+    gbc_slidera.gridx = 3;
+    gbc_slidera.gridy = 10;
+    pane.add(slidera, gbc_slidera);
+    setSlider(slidera, slideVal[2]);
+    
     listener(spinner2, slider, slideVal[2]);
+    listener(spinner2a, slidera, slideVal[2]);
     //////////////////////////////////////////////////////////
     JLabel label_6 = new JLabel("Torq (Nm)");
     label_6.setFont(new Font("Times New Roman", Font.BOLD, 20));
@@ -327,12 +391,30 @@ public class MiddlePanel extends JPanel implements ActionListener {
     GridBagConstraints gbc_slider_4 = new GridBagConstraints();
     gbc_slider_4.anchor = GridBagConstraints.WEST;
     gbc_slider_4.fill = GridBagConstraints.VERTICAL;
-    gbc_slider_4.insets = new Insets(0, 105, 0, 0);
+    gbc_slider_4.insets = new Insets(0, 90, 0, 0);
     gbc_slider_4.gridx = 4;
     gbc_slider_4.gridy = 10;
     pane.add(slider_4, gbc_slider_4);
     setSlider(slider_4, slideVal[3]);
+    
+
+    slider_4a = new JSlider(SwingConstants.VERTICAL, 0, 500, 0);
+    slider_4a.setPaintTicks(true);
+    slider_4a.setPaintLabels(true);
+    slider_4a.setMinorTickSpacing(20);
+    slider_4a.setMajorTickSpacing(100);
+    slider_4a.setUI(new MySliderUIBack(slider_4));
+    GridBagConstraints gbc_slider_4a = new GridBagConstraints();
+    gbc_slider_4a.anchor = GridBagConstraints.WEST;
+    gbc_slider_4a.fill = GridBagConstraints.VERTICAL;
+    gbc_slider_4a.insets = new Insets(0, 145, 0, 0);
+    gbc_slider_4a.gridx = 4;
+    gbc_slider_4a.gridy = 10;
+    pane.add(slider_4a, gbc_slider_4a);
+    setSlider(slider_4a, slideVal[3]);
+    
     listener(spinner3, slider_4, slideVal[3]);
+    listener(spinner3a, slider_4a, slideVal[3]);
 
   }
 
@@ -377,7 +459,46 @@ public class MiddlePanel extends JPanel implements ActionListener {
     }
   }
 
+  private static class MySliderUIBack extends BasicSliderUI {
 
+    private static float[] fracs = {0.0f, 0.4f, 0.7f, 1.0f};
+    private LinearGradientPaint p;
+
+    public MySliderUIBack(JSlider slider) {
+      super(slider);
+    }
+
+    @Override
+    public void paintTrack(Graphics g) {
+      Graphics2D g2d = (Graphics2D) g;
+      Rectangle t = trackRect;
+      Point2D start = new Point2D.Float(t.x - t.width, t.y - t.height);
+      Point2D end = new Point2D.Float(t.width, t.height);
+      Color[] colors = {Color.ORANGE, Color.red, Color.magenta, Color.pink,};
+      p = new LinearGradientPaint(start, end, fracs, colors);
+      g2d.setPaint(p);
+      g2d.fillRect(t.x, t.y, t.width, t.height);
+
+      // calculate how much of the progress bar to fill
+      double percentage = (double) slider.getValue() / (double) slider.getMaximum();
+
+
+      // PAINT THE FOREGROUND
+      // create the gradient paint
+      p = new LinearGradientPaint(start, end, fracs, colors);
+      g2d.setPaint(p);
+      g2d.fillRoundRect(t.x, t.y, t.width, (int) (t.height * percentage), 4, 4);
+      // PAINT THE BACKGROUND
+      // create a gradient paint for the background
+      p = new LinearGradientPaint(start, end, new float[] {0.4f, 0.8f},
+          new Color[] {Color.gray.brighter(), Color.gray.brighter().brighter()});
+      g2d.setPaint(p);
+      g2d.fillRoundRect(t.x, t.y, t.width, t.height - (int) (t.height * percentage), 4, 4);
+
+      // fill the progress bar with a rectange of that size, (with curved corners of 4px diameter)
+
+    }
+  }
 
   public void setSlider(JSlider tempslide, String tempvalue) {
     tempslide.setValue(Integer.parseInt(tempvalue));
